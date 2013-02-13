e5ac2245deee42bff09f7046f886054de4d36eff
mamoun.othman <artofeclipse@gmail.com>
2013-02-13 11:49:32 +0200

    update project to use bootstrap instead of twitter_bootstrap, make changes reqiured after moive to bootstrap, update contrib modules and core.

--- themes/vartheme/templates/page.tpl.php
+++ themes/vartheme/templates/page.tpl.php
@@ -1,45 +1,45 @@
 <header id="navbar" role="banner" class="navbar navbar-fixed-top">
   <div class="navbar-inner">
-  	<div class="container">
-  	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-  	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  	  </a>
-  	  
-  	  <?php if ($logo): ?>
-    		<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-    		  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-    		</a>
-  	  <?php endif; ?>
+    <div class="container">
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      </a>
+      
+      <?php if ($logo): ?>
+        <a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
-  	  <?php if ($site_name || $site_slogan): ?>
-    		<hgroup id="site-name-slogan">
-    		  <?php if ($site_name): ?>
-    			<h1>
-    			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
-    			</h1>
-    		  <?php endif; ?>
-    		</hgroup>
-  	  <?php endif; ?>
-  	  
-  	  <div class="nav-collapse">
-    	  <nav role="navigation">
-      		<?php if ($primary_nav): ?>
-      		  <?php print $primary_nav; ?>
-      		<?php endif; ?>
-      	  
-      		<?php if ($search): ?>
-      		  <?php if ($search): print render($search); endif; ?>
-      		<?php endif; ?>
-      		
-      		<?php if ($secondary_nav): ?>
-      		  <?php print $secondary_nav; ?>
-      		<?php endif; ?>
-    		</nav>
-  	  </div>         
-  	</div>
+      <?php if ($site_name || $site_slogan): ?>
+        <hgroup id="site-name-slogan">
+          <?php if ($site_name): ?>
+          <h1>
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+          </h1>
+          <?php endif; ?>
+        </hgroup>
+      <?php endif; ?>
+      
+      <div class="nav-collapse">
+        <nav role="navigation">
+          <?php if ($primary_nav): ?>
+            <?php print render($primary_nav); ?>
+          <?php endif; ?>
+          
+          <?php if (isset($search)): ?>
+            <?php print render($search); ?>
+          <?php endif; ?>
+          
+          <?php if ($secondary_nav): ?>
+            <?php print render($secondary_nav); ?>
+          <?php endif; ?>
+        </nav>
+      </div>         
+    </div>
   </div>
 </header>
 
@@ -52,16 +52,16 @@
 
     <?php print render($page['header']); ?>
   </header> <!-- /#header -->
-	
-	<div class="row">
-	  
+  
+  <div class="row">
+    
     <?php if ($page['sidebar_first']): ?>
       <aside class="span3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>  
-	  
-	  <section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
+    
+    <section class="<?php print _bootstrap_content_span($columns); ?>">  
       <?php if ($page['highlighted']): ?>
         <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
@@ -83,7 +83,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-	  </section>
+    </section>
 
     <?php if ($page['sidebar_second']): ?>
       <aside class="span3" role="complementary">
