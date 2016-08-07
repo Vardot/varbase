09c12ac91c0e877563dc63ab1511d72c2a36da9c
Rajab Natshah <r.natshah@vardot.com>
2016-08-07 12:27:37 +0300

    Issue #2780225: Integrated Varbase [8.x-4.x] with travis-ci to use Drupal 8 core Behat for Behavior and functional testing

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -2,7 +2,7 @@
 
 /**
  * @file
- * Contains \VarbaseContext.
+ * Contains \FeatureContext.
  */
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
@@ -11,7 +11,7 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 /**
  * Defines application features from the specific context.
  */
-class VarbaseContext extends RawDrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   /**
    * #varbase : To authenticat a user with a gavin username and password on the spot.
