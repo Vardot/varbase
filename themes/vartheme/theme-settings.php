ac342fc7ece3790ce1d4d52011ee2bf13ade09af
yasmeen_s <y.abuerrub@vardot.com>
2017-05-11 15:29:18 +0300

    Added Theme Settings for Vartheme (base theme) with header full-widt option

--- themes/vartheme/theme-settings.php
+++ themes/vartheme/theme-settings.php
@@ -13,31 +13,38 @@ use Drupal\Core\Url;
 /**
  * Implements hook_form_FORM_ID_alter().
  */
+
 function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
-  // Vertical tabs
+
+  //Vartheme Vertical tabs
   $form['vartheme'] = array(
     '#type' => 'vertical_tabs',
     '#prefix' => '<h2><small>' . t('Vartheme Settings') . '</small></h2>',
     '#weight' => -10,
   );
 
-  // General.
+  // General Tab setting for vatheme.
   $form['varthemegeneral'] = array(
     '#type' => 'details',
     '#title' => t('General'),
     '#group' => 'vartheme',
   );
-$form['varthemegeneral']['header'] = array(
+  
+  // header Subcontent inside the vartheme General tab.
+  $form['varthemegeneral']['header'] = array(
     '#type' => 'details',
     '#title' => t('Header'),
     '#collapsible' => TRUE,
     '#collapsed' => TRUE,
   );
-$form['varthemegeneral']['header']['vartheme_header_container'] = array(
+  
+  //Fluid container option.
+  $form['varthemegeneral']['header']['vartheme_header_container'] = array(
     '#type' => 'checkbox',
     '#title' => t('Header Fluid Container'),
     '#description' => t('Use <code>.container-fluid</code> class instead of <code>.container</code> class for Header region. <br /> See : @vartheme_link.', array('@vartheme_link' => Drupal::l('Fluid container' , Url::fromUri('http://getbootstrap.com/css/' , ['absolute' => TRUE , 'fragment' => 'grid-example-fluid'])),
     )),    
     '#default_value' => theme_get_setting('vartheme_header_container'),
   );
+  
 }
