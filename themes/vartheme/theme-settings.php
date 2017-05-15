63d1484e3fd28b6a48078847f96fc8035a06bafd
Rajab Natshah <rajabn@gmail.com>
2017-05-15 16:43:43 +0300

    Issue #2878061: Optimization for Vartheme Settings to work with VARTHEME_SUBTHEME too.

--- themes/vartheme/theme-settings.php
+++ themes/vartheme/theme-settings.php
@@ -27,6 +27,7 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
     '#title' => t('General'),
     '#group' => 'vartheme',
   );
+
   $form['vartheme_general']['header'] = array(
     '#type' => 'details',
     '#title' => t('Header'),
@@ -35,11 +36,11 @@ function vartheme_form_system_theme_settings_alter(&$form, FormStateInterface $f
   );
 
   // Adding check-box header fluid container.
-  $form['vartheme_general']['header']['vartheme_header_container'] = array(
+  $form['vartheme_general']['header']['header_container'] = array(
     '#type' => 'checkbox',
     '#title' => t('Header Fluid Container'),
     '#description' => t('Use <code>.container-fluid</code> class instead of <code>.container</code> class for Header region. <br /> See : @vartheme_link.', array('@vartheme_link' => Drupal::l('Fluid container' , Url::fromUri('http://getbootstrap.com/css/' , ['absolute' => TRUE , 'fragment' => 'grid-example-fluid'])),
-    )),    
-    '#default_value' => theme_get_setting('vartheme_header_container'),
+    )),
+    '#default_value' => theme_get_setting('header_container'),
   );
 }
