edaf8ba2e9ae2648bfabef9deda0572362557432
Rajab Natshah <rajabn@gmail.com>
2017-06-13 18:08:25 +0300

    Issue #2883671 by RajabNatshah: [Vartheme] Refactoring CSS/LESS structure in the Vartheme and VARTHEME_SUBTHEME themes to use SMACSS, and having each part as libraries

--- themes/vartheme/js/script.js
+++ themes/vartheme/js/script.js
@@ -8,25 +8,7 @@
 
   Drupal.behaviors.vartheme = {
     attach: function (context) {
-
-      // Update body top spacing with navebar fixed top.
-      function updateBodyTopSpacing() {
-        $('#toolbar-administration.toolbar').each(function () {
-          $(".main-container").css("padding-top", function() {
-            return $("#navbar.navbar-fixed-top").height() + 15;
-          });
-        });
-
-        $('#navbar.navbar-fixed-top').each(function () {
-          $(".main-container").css("padding-top", function() {
-            return $("#navbar.navbar-fixed-top").height() + 15;
-          });
-        });
-      }
-
-      updateBodyTopSpacing();
-      $(window).resize(updateBodyTopSpacing);
-
+      // Vartheme JavaScript behaviors goes here.
     }
   };
 
