7054266e9e2688a532420b890a2e5c3e9e37e465
Rajab Natshah <rajabn@gmail.com>
2017-10-08 17:17:15 +0300

    Issue #2911702: Update Varbase to use Drupal 8.4.0 - Fixed toolbar top space load of the page.

--- themes/vartheme/js/script.js
+++ themes/vartheme/js/script.js
@@ -12,6 +12,13 @@
       
       // Move panels ipe tray padding buttom by it's outer height.
       $('#panels-ipe-tray').parent('body').css('padding-bottom', $('#panels-ipe-tray').outerHeight());
+      
+      // Fix toolbar top space load of the page.
+      var toolbarTabOuterHeight = $('#toolbar-bar').find('.toolbar-tab').outerHeight() || 0;
+      var toolbarTrayHorizontalOuterHeight = $('.is-active.toolbar-tray-horizontal').outerHeight() || 0;
+      var topSpace = toolbarTabOuterHeight + toolbarTrayHorizontalOuterHeight;
+      $('body').css({ 'padding-top': topSpace });
+
     }
   };
 
