7ef581d9a00cb65f4a6f2a149f638ba6efa06056
yasmeen_s <y.abuerrub@vardot.com>
2018-01-24 10:19:44 +0200

    Varbase auth feature

---
+++ src/FormBit/varbase_auth.formbit.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @file
+ * FormBit file for varbase_auth feature mdoule.
+ */
+
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Get editable config names.
+ *
+ * @return array
+ *   Array of config names, and list of values.
+ */
+function varbase_auth_get_editable_config_names() {
+  $editable_cofnigs = [
+    'simple.settings' => [
+      'social_auth_type' => ['social_auth_google'],
+    ],
+  ];
+
+  return $editable_cofnigs;
+}
+
+/**
+ * Build form bit.
+ *
+ * @param array $formbit
+ *   FormBit for the form.
+ * @param FormStateInterface $form_state
+ *   Form status.
+ * @param array $install_state
+ *   Install state.
+ */
+function varbase_auth_build_formbit(array &$formbit, FormStateInterface &$form_state, array &$install_state = NULL) {
+  $formbit['social_auth_type'] = [
+    '#type' => 'checkboxes',
+    '#title' => t('Social authentications to enable'),
+    '#default_value' => ['social_auth_google'],
+    '#options' => [
+      'social_auth_google' => t('Google +'),
+      'social_auth_facebook' => t('Facebook'),
+      'social_auth_linkedin' => t('Linkedin'),
+      'social_auth_twitter' => t('Twitter'),
+    ],
+  ];
+}
+
+/**
+ * Submit form bit with editable config values.
+ *
+ * To update the editable config in drupal active config.
+ *
+ * @param array $editable_config_values
+ *   Editable cofnig values.
+ */
+function varbase_auth_submit_formbit(array $editable_config_values) {
+  $configFactory = \Drupal::configFactory()->getEditable('simple.settings');
+  $configFactory->set('social_auth_type', $editable_config_values['simple.settings']['social_auth_type']);
+  $configFactory->save(TRUE);
+}
