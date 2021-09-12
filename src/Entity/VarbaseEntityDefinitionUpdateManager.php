9f0836a70b5cce05e7c80790bdb1f0c4505c1d54
Rajab Natshah <rajabn@gmail.com>
2021-09-12 21:55:21 +0300

    Issue #3232568: Switch Varbase Entity Definition Update Manager class to extend the vardot/entity-definition-update-manager class library

--- src/Entity/VarbaseEntityDefinitionUpdateManager.php
+++ src/Entity/VarbaseEntityDefinitionUpdateManager.php
@@ -2,14 +2,8 @@
 
 namespace Drupal\varbase\Entity;
 
-use Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeListenerInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Field\FieldStorageDefinitionListenerInterface;
 use Vardot\Entity\EntityDefinitionUpdateManager;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+
 
 /**
  * Varbase Entity Definition Update Manager.
@@ -20,52 +14,5 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * https://github.com/Vardot/entity-definition-update-manager
  */
 final class VarbaseEntityDefinitionUpdateManager extends EntityDefinitionUpdateManager {
-
-  /**
-   * Constructs a new VarbaseEntityDefinitionUpdateManager.
-   *
-   * @param \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface $entity_definition_update_manager
-   *   The entity definition update manager.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   * @param \Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface $entity_last_installed_schema_repository
-   *   The last installed schema repository service.
-   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
-   *   The entity field manager service.
-   * @param \Drupal\Core\Entity\EntityTypeListenerInterface $entity_type_listener
-   *   The entity type listener interface.
-   * @param \Drupal\Core\Field\FieldStorageDefinitionListenerInterface $field_storage_definition_listener
-   *   The field storage definition listener service.
-   */
-  public function __construct(
-    EntityDefinitionUpdateManagerInterface $entity_definition_update_manager,
-    EntityTypeManagerInterface $entity_type_manager,
-    EntityLastInstalledSchemaRepositoryInterface $entity_last_installed_schema_repository,
-    EntityFieldManagerInterface $entity_field_manager,
-    EntityTypeListenerInterface $entity_type_listener,
-    FieldStorageDefinitionListenerInterface $field_storage_definition_listener
-  ) {
-    $this->entityDefinitionUpdateManager = $entity_definition_update_manager;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->entityLastInstalledSchemaRepository = $entity_last_installed_schema_repository;
-    $this->entityFieldManager = $entity_field_manager;
-    $this->entityTypeListener = $entity_type_listener;
-    $this->fieldStorageDefinitionListener = $field_storage_definition_listener;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    /** @noinspection PhpParamsInspection */
-    return new static(
-      $container->get('entity.definition_update_manager'),
-      $container->get('entity_type.manager'),
-      $container->get('entity.last_installed_schema.repository'),
-      $container->get('entity_field.manager'),
-      $container->get('entity_type.listener'),
-      $container->get('field_storage_definition.listener')
-    );
-  }
 
 }
