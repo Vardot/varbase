5958ed2acd68ca34d22acec084153edd379eec17
Rajab Natshah <rajabn@gmail.com>
2025-01-21 00:12:38 +0300

    Issue #3484640: Change the Form bit for Varbase AI to work with Drupal AI, and Keys

--- src/FormBit/varbase_ai.formbit.php
+++ src/FormBit/varbase_ai.formbit.php
@@ -15,9 +15,8 @@ use Drupal\Core\Form\FormStateInterface;
  */
 function varbase_ai_get_editable_config_names() {
   $varbase_ai_editable_configs = [
-    'openai.settings' => [
-      'api_key' => '',
-      'api_org' => '',
+    'key.key.open_ai_key' => [
+      'key_value' => '',
     ],
   ];
 
@@ -35,20 +34,12 @@ function varbase_ai_get_editable_config_names() {
  *   Install state.
  */
 function varbase_ai_build_formbit(array &$formbit, FormStateInterface &$form_state, array &$install_state = NULL) {
-  $formbit['api_key'] = [
+  $formbit['key_value'] = [
     '#type' => 'textfield',
     '#title' => ('API Key'),
-    '#default_value' => "sk-change-this",
+    '#default_value' => '',
     '#description' => t('The API key is required to interface with OpenAI services. Get your API key by signing up on the <a href=":link" target="_blank">OpenAI website</a>.', [':link' => 'https://openai.com/api']),
     '#element_validate' => ['validate_formbit_openai_api_key'],
-  ];
-
-  $formbit['api_org'] = [
-    '#type' => 'textfield',
-    '#title' => t('Organization ID'),
-    '#default_value' => "org-change-this",
-    '#description' => t('The organization ID on your OpenAI account. This is required for some OpenAI services to work correctly.'),
-    '#element_validate' => ['validate_formbit_openai_api_org'],
   ];
 
 }
@@ -62,15 +53,14 @@ function varbase_ai_build_formbit(array &$formbit, FormStateInterface &$form_sta
  *   Editable config values.
  */
 function varbase_ai_submit_formbit(array $editable_config_values) {
-  // Save the changed values for openai.settings config.
-  if (isset($editable_config_values['openai.settings'])
-    && isset($editable_config_values['openai.settings']['api_key'])
-    && isset($editable_config_values['openai.settings']['api_org'])) {
 
-    $openaiConfig = \Drupal::configFactory()->getEditable('openai.settings');
-    $openaiConfig->set('api_key', $editable_config_values['openai.settings']['api_key']);
-    $openaiConfig->set('api_org', $editable_config_values['openai.settings']['api_org']);
-    $openaiConfig->save(TRUE);
+  // Save the changed values for the OpenAI key config.
+  if (isset($editable_config_values['key.key.open_ai_key'])
+    && isset($editable_config_values['key.key.open_ai_key']['key_value'])) {
+
+    $aiKeyConfig = \Drupal::configFactory()->getEditable('key.key.open_ai_key');
+    $aiKeyConfig->set('key_provider_settings', ['key_value' => $editable_config_values['key.key.open_ai_key']['key_value']]);
+    $aiKeyConfig->save(TRUE);
   }
 }
 
@@ -85,19 +75,5 @@ function varbase_ai_submit_formbit(array $editable_config_values) {
 function validate_formbit_openai_api_key(array $element, FormStateInterface $form_state) {
   if ($form_state->getValue($element['#name']) == '') {
     $form_state->setErrorByName($element['#name'], t('The API key is required to interface with OpenAI services.'));
-  }
-}
-
-/**
- * Validate OpenAI API Organization ID.
- *
- * @param array $element
- *   A field array to validate.
- * @param \Drupal\Core\Form\FormStateInterface $form_state
- *   The current state of the form.
- */
-function validate_formbit_openai_api_org(array $element, FormStateInterface $form_state) {
-  if ($form_state->getValue($element['#name']) == '') {
-    $form_state->setErrorByName($element['#name'], t('Please, fill in the organization ID on your OpenAI account.'));
   }
 }
